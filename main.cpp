4ecaf591538e5d23e67c96c24015181b0aa99ba3
Vijitha Gunta <vijitha.gunta@sjsu.edu>
2024-04-13 23:49:53 -0700

    testing from loading from csv file

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <QApplication>
 #include "mainwindow.h"
 #include "Menu.h"
-#include "MenuItem.h"
+#include "MenuItemAdapter.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     Menu menu;
-    // Populate the menu
-    menu.addItem(MenuItem("Pizza", 9.99, "path/to/pizza.jpg", "Delicious pizza", Category::MainCourse, {DietaryRestriction::GlutenFree}, Availability::Available));
-    menu.addItem(MenuItem("Burger", 11.99, "path/to/burger.jpg", "Juicy burger", Category::MainCourse, {DietaryRestriction::DairyFree}, Availability::Available));
+    MenuItemAdapter adapter;
+
+    // Load items from a CSV file
+    std::vector<MenuItem> items = adapter.loadMenuItemsFromCSV("/Users/vijithagunta/menu-management/menuitems.csv");
+    for (const auto& item : items) {
+        menu.addItem(item);
+    }
 
     MainWindow mainWindow(&menu);
     mainWindow.show();
